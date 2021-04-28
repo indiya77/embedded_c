c0da99e888d3b3211802eb27bc2744ae0facfe84
indiya77 <indiyabhenly@gmail.com>
2021-04-28 21:04:14 +0530

    modified

--- inc/activity1.h
+++ inc/activity1.h
@@ -1,9 +1,13 @@
 #ifndef _ACTIVITY1_
 #define _ACTIVITY1_
 
+#ifndef __AVR_ATmega328__
+    #define __AVR_ATmega328__
+#endif
+
 #include <avr/io.h>
 
 void pin_init();
-void delay_loop();
+void activity1_loop();
 
 #endif
