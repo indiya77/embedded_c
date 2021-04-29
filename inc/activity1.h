b3ecece6758a1b651d1bf67ada2f1a74cbf7049c
indiya77 <indiyabhenly@gmail.com>
2021-04-29 12:05:41 +0530

    modified activity1

--- inc/activity1.h
+++ inc/activity1.h
@@ -5,9 +5,15 @@
     #define __AVR_ATmega328__
 #endif
 
+#define LED_PORT (PORTB) // led port number'
+#define LED_PIN (PB0) // led pin number
+#define DRIVER_PIN (PC6) // to check whether the driver is seated
+#define HEATER_PIN (PD4) // to check whether the heater is turned on
+
 #include <avr/io.h>
 
 void pin_init();
 void activity1_loop();
+void activity_1();
 
 #endif
--- main.c
+++ main.c
@@ -1,16 +1,10 @@
 #include <avr/io.h>
-#include <util/delay.h>
 
 #include "activity1.h"
 #include "activity2.h"
 
 int main(){
-    uint16_t data;
-    pin_init();
-
-    InitAdc();
-    while(1){
-        activity1_loop();
-        data = ReadAdc(0);
-    }
+    activity_1();
+    activity_2();
+    return 0;
 }
